334430704e1262053a8c38f0edb5faf864aec39b
timbuendert <tim.moritz.buendert@googlemail.com>
2021-12-19 18:22:38 +0100

    Life saver

--- app.py
+++ app.py
@@ -11,6 +11,29 @@ from src.utils_dash import _player_selector
 import recommmendation_engine
 from hotzone import hotzone
 
+import dash
+import wikipediaapi
+import requests
+from PIL import Image
+from dash import dcc, html
+from dash import dash_table
+from dash.dependencies import Input, Output, State
+import plotly.express as px
+import plotly.graph_objs as go
+import pandas as pd
+from src.get_data import get_clean_player_data, get_team_image
+from src.utils_dash import _player_selector, _team_selector, _link_team_website, _team_full_name, _get_team_id, \
+    _get_mvp_id_team, _player_full_name, _mvp_descr_builder
+import dash
+import wikipediaapi
+from dash import dcc, html
+from dash import dash_table
+from dash.dependencies import Input, Output
+import plotly.express as px
+from src.get_data import get_clean_player_data
+from src.utils_dash import _player_selector
+import recommmendation_engine
+
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, title="NBA GM")
@@ -20,15 +43,75 @@ app = dash.Dash(__name__, title="NBA GM")
 
 # SETUP STATIC DATA
 player_selector = _player_selector()
+team_selector = _team_selector()
 player_data = recommmendation_engine.get_players_data()
 team_data = recommmendation_engine.get_teams_data()
 
-# APP LAYOUT
 
+# APP ELEMENTS
+offcanvas = html.Div(
+    [
+        dbc.Button("Further Team Information", id="teamselect-open-offcanvas", n_clicks=0, color='light'),
+        dbc.Offcanvas([html.Div(
+            html.P(id='teamselect-output-wiki'
+                   )),
+            dbc.Button(id="teamselect-link-button",
+                       target="_blank",
+                       color="info", external_link=True)],
+            backdrop=True,
+            scrollable=True,
+            id="offcanvas",
+            is_open=False,
+            placement='top'
+        ),
+    ]
+)
+col_teamname = dbc.Col(html.Div(
+    [html.H2(id='teamselect-output-container',
+             className="display-3", style={'margin': 'auto', 'width': '100%', 'display': 'inline-block'}), offcanvas]),
+    md=10)
+col_logo = dbc.Col(html.Div(
+    [html.Img(id='teamselect-image', style={'margin': 'auto', 'width': '100%', 'display': 'inline-block'})]), md=2)
+starplayer = dbc.Alert(
+    [
+        html.H4("Starplayer", className="alert-heading"),
+        html.P(id='teamselect-mvp-descr'),
+        html.Hr(),
+        html.P(
+            id='teamselect-mvp-name',
+            className="mb-0",
+        ),
+    ]
+)
+right_part = dbc.Col([html.Div(
+    [html.Img(id='teamselect-mvp-image', style={'margin': 'auto', 'width': '50%', 'display': 'inline-block'})]),
+    starplayer],
+    md=4)
+left_jumbotron = dbc.Col([dbc.Row([col_teamname, col_logo], className="align-items-md-stretch"),
+                          html.Hr(className="my-2"),
+                          dcc.Dropdown(
+                              id='teamselect-dropdown',
+                              options=[{'label': team,
+                                        'value': team_selector.iloc[
+                                            i, 1]} for i, team in
+                                       enumerate(
+                                           team_selector.label.unique())],
+                              value='ATL'
+                          ), html.Hr(className="my-2"),
+                          dbc.Container([
+                              dcc.Graph(id='teamselect-capspace-graph')
+                          ])], md=8, className="h-100 p-5 bg-light border rounded-3")
+jumbotron = dbc.Row(
+    [left_jumbotron, right_part],
+    className="align-items-md-stretch",
+)
+
+
+# APP LAYOUT
 app.layout = html.Div(children=[
     dcc.Tabs(id='tabs-example', value='tab-1', children=[
         dcc.Tab(label='Player Bio', value='tab-1', children=[
-            html.H1(children='NBA GM'),
+            html.H2(children='Player', className="display-3"), #html.H1(children='NBA GM'),
             dbc.Row([dbc.Col(html.Div([html.Div([html.Img(id='playerselect-image')])], style={'width': '49%'})),
 
                      dbc.Col(html.Div([html.Div([html.Img(id='teamSel-image')])], style={'width': '49%'}))
@@ -57,7 +140,7 @@ app.layout = html.Div(children=[
         ]),
 
         dcc.Tab(label='Recommendation engine', value='tab-3', children=[
-            html.H1(children='Recommendation Engine for NBA players'),
+            html.H2(children='Recommendation Engine for NBA players', className="display-3"),
             html.Div([html.Div(
                 [html.Img(id='teamRep-image')]
 
@@ -84,24 +167,15 @@ app.layout = html.Div(children=[
 
                 )], style={'width': '20%'}
             ),
-            dcc.Loading(children=[html.Div(id='teamRec-player-dropdown')], fullscreen=False, type='dot',
-                        color="#119DFF"),
-            html.Div([html.Div(
-                [html.Img(id='playerRec-image')]
-            )], style={'width': '49%'})
-        ]
-
-                ),
             dcc.Loading(children = [html.Div(id='teamRec-player-dropdown')], fullscreen=False, type='dot', color="#119DFF"),
             html.Div([html.Div(
                 [html.Img(id='playerRec-image')]
             )], style={'width': '49%'})
-            ]
-        ),
+            ]),
 
         dcc.Tab(label='Dimensionality Reduction', value='tab-4', children=[
 
-            html.H1(children='Projections of active NBA players into 2D'),
+            html.H2(children='Projections of active NBA players into 2D', className="display-3"),
 
             html.Div(
                 [dcc.Dropdown(
@@ -120,11 +194,16 @@ app.layout = html.Div(children=[
                             color="#119DFF")
 
             ])
-            ]
-        )
-    ])
 
-])
+            ]
+        ),
+        dcc.Tab(label='Team', value='tab-5', children=[jumbotron])
+        ], colors={
+        "border": "white",
+        "primary": "#17408b",
+        "background": "white"})
+    ]) 
+
 
 
 # APP CALLBACKS
@@ -134,6 +213,40 @@ app.layout = html.Div(children=[
     Input('playerselect-dropdown', 'value'))
 def update_output(value):
     return f'Player has the ID: {value}'
+
+@app.callback(
+    [Output('teamselect-output-container', 'children'),
+     Output('offcanvas', 'title')],
+    Input('teamselect-dropdown', 'value'))
+def update_output(value):
+    return _team_full_name(value), _team_full_name(value)
+
+@app.callback(
+    [dash.dependencies.Output('teamselect-mvp-image', 'src'),
+     dash.dependencies.Output('teamselect-mvp-descr', 'children'),
+     dash.dependencies.Output('teamselect-mvp-name', 'children')],
+    [dash.dependencies.Input('teamselect-dropdown', 'value')])
+def update_output(value):
+    team_id = _get_team_id(value)
+    mvp_data, url_image = _get_mvp_id_team(team_id=team_id, season='2020-21')
+    mvp_name, mvp_pos = _player_full_name(player_id=mvp_data[0])
+    descr = _mvp_descr_builder(mvp_name=mvp_name, mvp_position=mvp_pos, mvp_data=mvp_data)
+    return url_image, descr, mvp_name
+
+@app.callback(
+    Output('teamselect-link-button', 'children'),
+    Input('teamselect-dropdown', 'value'))
+def update_output(value):
+    full_name = _team_full_name(value)
+    return f'Visit the {full_name}\'s website.'
+
+@app.callback(
+    Output('teamselect-link-button', 'href'),
+    Input('teamselect-dropdown', 'value'))
+def update_output(value):
+    nickname = _link_team_website(value)
+    return f"https://www.nba.com/{nickname}/"
+
 
 @app.callback(
     Output('teamSel-image', 'src'),
@@ -167,6 +280,38 @@ def update_player(value):
     [Input('playerselect-dropdown', 'value')])
 def update_image_src(value):
     return f"https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{str(value)}.png"
+
+
+@app.callback(
+    dash.dependencies.Output('teamselect-image', 'src'),
+    [dash.dependencies.Input('teamselect-dropdown', 'value')])
+def get_team_image(value, season: str = '2021-22'):
+    """
+    :param team_abb:
+    :return:
+    """
+    url = f"http://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/{value.lower()}.png"
+    return url
+
+
+@app.callback(
+    dash.dependencies.Output('teamselect-output-wiki', 'children'),
+    [dash.dependencies.Input('teamselect-dropdown', 'value')])
+def _team_wiki_summary(value):
+    wiki_wiki = wikipediaapi.Wikipedia('en')
+    full_name = _team_full_name(value)
+    page_py = wiki_wiki.page(full_name)
+    return page_py.summary
+
+@app.callback(
+    Output("offcanvas", "is_open"),
+    Input("teamselect-open-offcanvas", "n_clicks"),
+    [State("offcanvas", "is_open")],
+)
+def toggle_offcanvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
 
 @app.callback(
     Output('playerselect-output-container-wiki', 'children'),
@@ -260,6 +405,12 @@ def selected_player(value):
 def update_image_recPlayer(children):
     player_id = list(player_data[player_data['player_names'] == children]['id'])[0]
     return f"https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{str(player_id)}.png"
+
+@app.callback(
+    Output('teamselect-capspace-graph', 'figure'),
+    Input('teamselect-dropdown', 'value'))
+def update_output(value):
+    return recommmendation_engine.visualize_capspace_team_plotly(value)
 
 
 if __name__ == '__main__':
